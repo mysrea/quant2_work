b0379b09e11b700195775c8a0823283d39320ca7
mysrea <100560337@alumnos.uc3m.es>
2026-02-05 12:42:39 +0100

    Plot

--- assignment1/ass1plot.R
+++ assignment1/ass1plot.R
@@ -1,2 +1,9 @@
+library(gapminder)
 library(ggplot2)
-gapminder<-read.csv("data/gapminder.csv")
+gapminder<-read.csv("C:/Users/zebra/Pictures/uc3m/SPRING 2026/quant 2/quant2_work/assignment1/data/gapminder.csv")
+#
+head(gapminder)
+str(gapminder)
+#
+countries<-c("Spain","Brazil","Canada","Chile")
+df<-gapminder[gapminder$country%in%countries,]
