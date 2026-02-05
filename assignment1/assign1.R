8dc618514d3f0f63085f808dba64759010107832
mysrea <100560337@alumnos.uc3m.es>
2026-02-05 12:30:37 +0100

    Installing gapminder, creating csv, files

---
+++ assignment1/assign1.R
@@ -0,0 +1,6 @@
+install.packages("gapminder")
+#
+library(gapminder)
+#
+write.csv(gapminder,"C:/Users/zebra/Pictures/uc3m/SPRING 2026/quant 2/quant2_work/assignment1/data/gapminder.csv",row.names=FALSE)
+#
