7ab1b49ef5a5a1a4337b6067beadf51732d1e310
mysrea <100560337@alumnos.uc3m.es>
2026-02-26 12:50:11 +0100

    classwork wip

---
+++ assignment4/assign4r.R
@@ -0,0 +1,13 @@
+
+# Setup -------------------------------------------------------------------
+library(tidyverse)
+library(modelsummary)
+library(marginaleffects)
+library(readstata13)
+library(haven)
+
+
+# Classwork ---------------------------------------------------------------
+
+corruption <- read_dta("corruption.dta")
+
