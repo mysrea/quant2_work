bd7969faea4d89e475ac433f97d9b5a93761cca0
mysrea <100560337@alumnos.uc3m.es>
2026-03-19 12:15:56 +0100

    classwork done

---
+++ assignment7/assign7r.R
@@ -0,0 +1,16 @@
+
+# Setup -------------------------------------------------------------------
+
+library(tidyverse)
+library(modelsummary)
+library(marginaleffects)
+library(haven)
+library(fixest)
+library(plm)
+library(did)
+library(sf)
+
+
+# Classwork ---------------------------------------------------------------
+
+
