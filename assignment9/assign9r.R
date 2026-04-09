023286632e25500d946ddb88a94c81750b349c8f
mysrea <100560337@alumnos.uc3m.es>
2026-04-09 11:57:09 +0200

    assignment 9 setup

---
+++ assignment9/assign9r.R
@@ -0,0 +1,19 @@
+
+# Setup -------------------------------------------------------------------
+
+library(carData)
+library(MASS)
+library(nnet)
+library(pscl)
+library(AER)
+library(marginaleffects)
+library(ggplot2)
+library(survival)
+library(broom)
+data(BEPS)
+
+
+# Classwork ---------------------------------------------------------------
+
+
+
